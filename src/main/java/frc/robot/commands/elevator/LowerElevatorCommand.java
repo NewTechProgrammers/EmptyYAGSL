3a7b558479aa0d46fbbe548ec9045b7c221ed737
checkthistape <litos.art2006@gmail.com>
2025-02-19 11:08:31 +0100

    Added commands folder and LowerElevatorCommand & RaiseElevatorCommand commands

---
+++ src/main/java/frc/robot/commands/elevator/LowerElevatorCommand.java
@@ -0,0 +1,41 @@
+package frc.robot.commands.elevator;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+import java.util.Set;
+
+import frc.robot.subsystems.Elevator;
+
+public class LowerElevatorCommand extends Command {
+    private final Elevator elevator;
+
+    public LowerElevatorCommand(Elevator elevator) {
+        this.elevator = elevator;
+    }
+
+    @Override
+    public void initialize() {       
+    }
+
+    @Override
+    public void execute() {
+        elevator.runDown();
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        elevator.stop();
+    }
+
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    public Set<Subsystem> getRequirements() {
+        return Set.of(elevator);
+    }
+    
+}
