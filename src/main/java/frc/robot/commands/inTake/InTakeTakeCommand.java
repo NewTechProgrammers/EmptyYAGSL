ad766f1cd2e8dc6c9ff7dcbe745e6d041686f51e
Tinilinsk <tema.ryabkov@gmail.com>
2025-02-27 16:00:47 +0100

    Take and shoot commands for Intake

--- src/main/java/frc/robot/commands/inTake/InTakeTakeCommand.java
+++ src/main/java/frc/robot/commands/inTake/InTakeTakeCommand.java
@@ -2,24 +2,53 @@ package frc.robot.commands.inTake;
 
 import java.util.Set;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
+import frc.robot.Constants.MechanismConstants;
 import frc.robot.subsystems.Intake;
+
 
 public class IntakeTakeCommand extends Command{
     private final Intake intake;
+
+    private boolean pieceDetected = false;
+    private boolean shootmod = false;
 
     public IntakeTakeCommand(Intake intake) {
         this.intake = intake;
     }
 
     @Override
-    public void initialize() {       
+    public void initialize() {
+        if (pieceDetected) {
+            shootmod = true;
+        } else {
+            shootmod = false;
+        }
     }
+
+    
 
     @Override
     public void execute() {
-        intake.take();
+        if (shootmod) {
+            intake.shoot();
+            if (!intake.intakeLimitSwitch()) {
+                pieceDetected = false;
+                shootmod = false;
+            }
+        }
+        else {
+            if (intake.intakeLimitSwitch()) {
+                intake.stop();
+                pieceDetected = true;
+            } else {
+                intake.take();
+                pieceDetected = false;
+            }
+        }
+        
     }
 
     @Override
